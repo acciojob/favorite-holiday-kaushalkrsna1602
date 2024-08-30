74a460d5508e6015d846dc5134c9c0b3d5e3d492
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-08-30 07:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+cy.get('body').then(($body) => {
+    if ($body.find('i').length) {
+        cy.get('i').should('be.visible');
+    } else {
+        // You can log an error or handle the condition
+        cy.log('i element not found');
+    }
+});
